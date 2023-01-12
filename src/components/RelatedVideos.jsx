a089990d6d8a1c77b419f4e7480e8982b1073168
jiwook <kjo2354@naver.com>
2023-01-12 18:21:21 +0900

    Initial commit

---
+++ src/components/RelatedVideos.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import VideoCard from '../components/VideoCard';
+import {useQuery} from '@tanstack/react-query';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
+
+export default function RelatedVideos({id}) {
+  const {youtube} = useYoutubeApi();
+  const {isLoading, error, data:videos} = useQuery(['related', id], () => youtube.relatedVideos(id), {
+    staleTime: 1000 * 60 * 5,
+  });
+  
+  return <>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong</p>}
+      {videos && <ul>
+        { videos.map(video => 
+        <VideoCard key={video.id} video={video} type='list'/>
+        )}
+        </ul>}
+  </>
+}
